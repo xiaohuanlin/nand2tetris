5d897243dbbe7d5f8bd82bd8040496bc9df72350
xiaohuanlin <123971348@qq.com>
2021-02-08 16:42:31 +0800

    project 4 done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,69 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+// address 24576 reflect keyboard
+// address 16384 - 24575 reflect screen
+
+(LOOP)
+@16384
+D=A // D = 16384
+
+@i
+M=D // i = 16384
+
+@24576
+D=M // D = M[24576]
+
+@ELSE
+D;JGT // if (M[24576] > 0) goto ELSE
+
+(INNER_LOOP)
+@i
+D=M; // D = i
+
+@24575
+D=D-A; // D = i - 24575
+
+@END
+D;JGT // if (i - 24575) > 0 goto END
+
+@i
+D=M 
+A=D
+M=0 // M[i] = 0
+
+@i
+M=M+1 // i = i + 1
+
+@INNER_LOOP
+0;JMP
+
+(ELSE)
+
+(INNER_ELSE_LOOP)
+@i
+D=M; // D = i
+
+@24575
+D=D-A; // D = i - 24575
+
+@END
+D;JGT // if (i - 24575) > 0 goto END
+
+@i
+D=M 
+A=D
+M=-1 // M[i] = -1 this is 0x1111
+
+@i
+M=M+1 // i = i + 1
+
+@INNER_ELSE_LOOP
+0;JMP
+
+(END)
+
+@LOOP
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,42 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@i
+M=1 // i = 1
+
+@res
+M=0 // res = 0
+
+(LOOP)
+@i
+D=M // D = i
+
+@0
+D=D-M // D = i - M[0]
+
+@END
+D;JGT // if (i - M[0]) > 0 goto END
+
+@1
+D=M // D = M[1]
+
+@res
+M=D+M // res = M[1] + res
+
+@i
+M=M+1 // i = i + 1
+
+@LOOP
+0;JMP // goto LOOP
+
+(END)
+
+@res
+D=M
+
+@2
+M=D
+
+@END
+0;JMP // endless loop
